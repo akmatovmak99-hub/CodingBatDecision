96c99a8c71786f116a5cd04b77d52ffeafaffcc9
Beka <akmatovmak99@gmail.com>
2026-07-18 13:46:23 +0600

    FontTimes

---
+++ src/main/java/Warmup2/FontTimes.java
@@ -0,0 +1,21 @@
+package Warmup2;
+
+public class FontTimes {
+    public static void main(String[] args) {
+
+    }
+    public String frontTimes(String str, int n) {
+        int frontLen = 3;
+        if (frontLen > str.length()) {
+            frontLen = str.length();
+        }
+        String front = str.substring(0, frontLen);
+
+        String result = "";
+        for (int i=0; i<n; i++) {
+            result = result + front;
+        }
+        return result;
+    }
+
+}
