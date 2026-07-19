ab89a4ac7629aa6da864b88ad9abd4805da09af4
Beka <akmatovmak99@gmail.com>
2026-07-20 02:31:39 +0600

    String1 and HelloName

---
+++ src/main/java/String1/HelloName.java
@@ -0,0 +1,12 @@
+package String1;
+
+public class HelloName {
+    public static void main(String[] args) {
+        HelloName a = new HelloName();
+        System.out.println("Beka");
+    }
+    public String helloName(String name) {
+        return "Hello "+name+"!";
+    }
+
+}
