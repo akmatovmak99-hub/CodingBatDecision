a63e1c862f588ac843091feba1905e8fc9ae2050
Beka <akmatovmak99@gmail.com>
2026-07-19 23:52:56 +0600

    StringMatch

---
+++ src/main/java/Warmup2/StringMatch.java
@@ -0,0 +1,22 @@
+package Warmup2;
+
+public class StringMatch {
+    public static void main(String[] args) {
+        StringMatch a =new StringMatch();
+        System.out.println(a.stringMatch("xxcaazz", "xxbaaz));
+    }
+    public int stringMatch(String a, String b) {
+        int len = Math.min(a.length(), b.length());
+        int count = 0;
+        for (int i=0; i<len-1; i++) {
+            String aSub = a.substring(i, i+2);
+            String bSub = b.substring(i, i+2);
+            if (aSub.equals(bSub)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
